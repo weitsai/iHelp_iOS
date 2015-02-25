b6f9f82520475378c305bdcd26ef6e7ac4c063c8
Birdy Chang <changbirdy@gmail.com>
2015-02-25 14:47:18 +0800

    Initial Commit

---
+++ iHelp_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iHelp_iOS
+//
+//  Created by Birdy Chang on 2015/2/25.
+//  Copyright (c) 2015年 Birdy Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
